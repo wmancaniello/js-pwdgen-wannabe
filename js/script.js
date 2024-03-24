5d4a62fcc5566b3e6868bfad610de1fec0f6de8a
wmancaniello <wmancaniello@gmail.com>
2024-03-24 11:49:47 +0100

    Finish exercise

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Test
+// alert("prova JS");
+
+
+// Chiedere all'utente nome, cognome, colore preferito e generare una password
+
+
+// RACCOLTA DATI E DICHIARAZIONE tramite prompt
+// Nome
+const userName = prompt("Benvenuto utente! Qual'è il tuo nome?"); //string
+console.log(userName, typeof userName);
+
+// Cognome
+const userSurname = prompt(`Piacere ${userName}, ora mi indicheresti il tuo cognome?`); //string
+console.log(userSurname, typeof userSurname);
+
+// Colore preferito
+const favoriteColor = prompt(`Perfetto ${userName} ${userSurname}, ora puoi indicarmi, qual è il tuo colore preferito?`); //string
+console.log(favoriteColor, typeof favoriteColor);
+
+// Numero random da 1 a 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber, typeof randomNumber);
+
+
+// CONCATENAZIONE A CUI DO UNA COSTANTE
+const newPassword = `La tua nuova password sarà: ${userName}${userSurname}${favoriteColor}${randomNumber}`; //string
+console.log(newPassword, typeof newPassword);
+
+// STAMPA DELLA PASSWORD GENERATA IN PAGINA
+document.getElementById("result").innerHTML = newPassword;
+
+
+
